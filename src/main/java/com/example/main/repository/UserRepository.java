4581f0de87b6b7a98b6e70b24a501f00d6b84c2b
Brii <145430813+Brii359@users.noreply.github.com>
2025-02-03 00:22:39 +0800

    Update UserRepository.java

--- src/main/java/com/example/main/repository/UserRepository.java
+++ src/main/java/com/example/main/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.example.main.repository;
 import com.example.main.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 }
